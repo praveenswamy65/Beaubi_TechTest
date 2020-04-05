9beabfca3e29833b59bf6423bc98cbece5999d36
praveen swamy <praveenswamy65@gmail.com>
2020-04-05 14:06:21 +0200

    second initial

--- src/components/Forms/Addproducts.js
+++ src/components/Forms/Addproducts.js
@@ -1,5 +1,5 @@
 //import React from 'react';
-const {Pool, Client} = require('pg')
+/*const {Pool, Client} = require('pg')
 const connectionString = 'postgres://pvnbotlxjwpmcy:12463bc7f7c177094af39b1b3d28afd1a97eab88bf2d7d6b37c0864a0fbe0bde@ec2-52-87-58-157.compute-1.amazonaws.com:5432/d130271iijru7i'
 const pool = new Pool({
     connectionString: connectionString,
@@ -25,6 +25,7 @@ async function getNewArrivals(request, response) {
 /*'module.export' allows multiple functions to be exported at the same time! No need to declare
  one by one (ie export const deleteUser(){}).
  With ES6 syntax, its getUsers instead of getUsers:getUsers*/
-module.exports = {
+/*module.exports = {
     getNewArrivals,
 }
+*/
