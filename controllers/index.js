7a360b2af391870624b56ba70c8fc54337b83017
praveen swamy <praveenswamy65@gmail.com>
2020-04-06 16:25:11 +0200

    second commit

--- controllers/index.js
+++ controllers/index.js
@@ -1,12 +1,14 @@
 const models = require('../models').brands;
+const reqProds = require('../src/components/Forms/ProductReq');
+
 var Create=function (req,res) {
   models.create({
     //brand_name: req.brand_name,//req.formData.brand,
     //product:  req.product_name,//req.formData.prod,
     //category: req.category//req.formData.cat
-    brand_name : this.props.brand_name,
-    product : this.props.product_name,
-    category:this.props.category
+    brand_name : reqProds.state.brand_name,
+    product : reqProds.state.product_name,
+    category:reqProds.state.category
   }, function (err, usr) {
     if (err) console.log("Error in updating the product.");
     else {
