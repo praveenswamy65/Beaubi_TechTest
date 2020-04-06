5fd1595f134372c51e6d1c58d0a27f662d549f78
praveen swamy <praveenswamy65@gmail.com>
2020-04-06 05:03:04 +0200

    second commit

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,11 @@
 const models = require('../models').brands;
 var Create=function (req,res) {
+  alert(req.body());
   models.create({
 
-    brand_name: req.formData.brand,
-    product:req.formData.prod,
-    category: req.formData.cat
+    //brand_name: req.formData.brand,
+    //product:req.formData.prod,
+    //category: req.formData.cat
 
   }, function (err, usr) {
     if (err) console.log("Error in updating the product.");
